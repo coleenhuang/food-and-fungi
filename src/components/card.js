25ee50755082d55bfe8ba6c45f91b4fef1406538
Coleen Huang <coleenhuang@gmail.com>
2021-02-22 10:52:52 -0800

    added images to index and transitions on hover

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './card.module.scss';
+import CardImage from './cardImage';
 
 const Card = (props) => {
     return (
     <div className={styles.indexCard}>
         <Link to={`/${props.name.toLowerCase()}`}>
-        {props.name}
+            <CardImage type={props.name.toLowerCase()}/>
+            <h2>{props.name}</h2>
         </Link>
     </div>)
 }
