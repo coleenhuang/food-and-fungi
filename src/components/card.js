ae4b9f2a3e7b1c01b61216c331b022adc8f5a7b7
Coleen Huang <coleenhuang@gmail.com>
2021-02-22 11:52:35 -0800

    deleted cardImage file

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import styles from './card.module.scss';
-import CardImage from './cardImage';
+import Img from 'gatsby-image';
 
 const Card = (props) => {
+    const data = useStaticQuery(graphql`
+    query {
+        mushrooms: file(relativePath: {eq: "mushrooms.jpg"}) {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          molds: file(relativePath: {eq: "mold.jpg"}) {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          yeasts: file(relativePath: {eq: "yeast.jpg"}) {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+    }`)
     return (
     <div className={styles.indexCard}>
         <Link to={`/${props.name.toLowerCase()}`}>
-            <CardImage type={props.name.toLowerCase()}/>
+            <Img fluid={data.mushrooms.childImageSharp.fluid}/>
             <h2>{props.name}</h2>
         </Link>
     </div>)
